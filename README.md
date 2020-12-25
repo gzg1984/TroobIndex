071299809e4276c7aa855aa5360d0819a6b318ec
Zhigang Gao <gzg1984@aliyun.com>
2020-12-25 16:29:42 +0800

    Finish Index Search Feature

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ src/*
 ```
 mvn package -q
 ```
+
+## How to Use the Jar file directyly
+```
+java -jar index-0.0.3-SNAPSHOT.jar -h
+java -jar index-0.0.3-SNAPSHOT.jar -q Copyright
+
+```
--- src/main/java/index/index.java
+++ src/main/java/index/index.java
@@ -1,21 +1,12 @@
 package index;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
 
 import java.nio.file.Paths;
 import org.apache.lucene.store.FSDirectory;
@@ -28,220 +19,155 @@ import org.apache.lucene.search.highlight.TokenSources;
 import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
 import org.apache.lucene.search.highlight.Fragmenter;
 
-
 import org.apache.lucene.search.highlight.QueryScorer;
 import org.apache.lucene.queryparser.classic.QueryParser;
-//import org.apache.lucene.util.Version;
-
 
 import java.io.IOException;
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+
 public class index {
+    static boolean verbose = false;
+    static String file = "/opt/file_root/index_base/spdk_v17_10_1/";
 
-    public static void main(String[] args) throws IOException {
-       
-        queryRAM(args);
-        querySPDK(args);
-        hilightSPDK(args);
-    }
+    static String queryString = "LICENSE";
 
-    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-        Document doc = new Document();
-        doc.add(new TextField("title", title, Field.Store.YES));
+    static String field = "content";
 
-        // use a string field for isbn because we don't want it tokenized
-        doc.add(new StringField("isbn", isbn, Field.Store.YES));
-        w.addDocument(doc);
-    }
-    private static void queryRAM(String[] args) throws IOException{
-                // 0. Specify the analyzer for tokenizing text.
-        //    The same analyzer should be used for indexing and searching
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-
-        // 1. create the index
-        Directory index = new RAMDirectory();
-
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817");
-        addDoc(w, "Lucene for Dummies", "55320055Z");
-        addDoc(w, "Managing Gigabytes", "55063554A");
-        addDoc(w, "The Art of Computer Science", "9900333X");
-        w.close();
-
-        // 2. query
-        String queryString = args.length > 0 ? args[0] : "lucene";
-
-        // the "title" arg specifies the default field to use
-        // when no field is explicitly specified in the query.
-        Query query = null;
+    private static void handleOpthins(String[] args) throws IOException {
+        CommandLineParser parser = new BasicParser();
+        Options options = new Options();
+        options.addOption("h", "help", false, "Print this usage information");
+        options.addOption("v", "verbose", false, "Print out VERBOSE information");
+        options.addOption("f", "file", true, "File to save program output to");
+        options.addOption("q", "query", true, "Key word to query");
+        // Parse the program arguments
         try {
-            String[] fields = {"title"};
-            query = new MultiFieldQueryParser(fields, analyzer).parse(queryString);
-        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-            e.printStackTrace();
-        }
+            CommandLine commandLine = parser.parse(options, args);
+            // Set the appropriate variables based on supplied options
 
-        // 3. search
+            if (commandLine.hasOption('h')) {
+                System.out.println("Help Message");
+                System.exit(0);
+            }
+            if (commandLine.hasOption('v')) {
+                verbose = true;
+            }
+            if (commandLine.hasOption('f')) {
+                file = commandLine.getOptionValue('f');
+            }
 
-        int hitsPerPage = 10;
-        //IndexReader reader = DirectoryReader.open(index);
-        IndexReader reader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-        //searcher.search(query, collector);
-        searcher.search(query, collector);
-        ScoreDoc[] hits = collector.topDocs().scoreDocs;
-
-        // 4. display results
-        System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". Title " + d.get("title") + "\t" + d.get("isbn") ) ;
-        }
-
-        // reader can only be closed when there
-        // is no need to access the documents any more.
-        reader.close();
-    }
-
-    private static void querySPDK(String[] args) throws IOException{
-        Analyzer lxranalyzer = new SourceFileAnalyzer();
-        Query lxrquery = null;
-        String lxrqueryString = args.length > 0 ? args[0] : "LICENSE";
-
-        try {
-            String[] fields = {"content"};
-            lxrquery = new MultiFieldQueryParser(fields, lxranalyzer).parse(lxrqueryString);
-        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-            e.printStackTrace();
-        }
-        String path = "/opt/file_root/index_base/spdk_v17_10_1/";
-        Directory diskIndex = FSDirectory.open(Paths.get(path));
-
-
-
-
-        int hitsPerPage = 10;
-        //IndexReader reader = DirectoryReader.open(index);
-        IndexReader reader = DirectoryReader.open(diskIndex);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-        //searcher.search(query, collector);
-        searcher.search(lxrquery, collector);
-        ScoreDoc[] hits = collector.topDocs().scoreDocs;
-
-        // 4. display results
-        System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". File " + d.get("filePath") + "\t" + d.get("fileName")  + "\t" + d.get("projectId")) ;
-        }
-
-        // reader can only be closed when there
-        // is no need to access the documents any more.
-        reader.close();
-    }
-
-    private static void hilightSPDK(String[] args) throws IOException{
-        Analyzer lxranalyzer = new SourceFileAnalyzer();
-        Query lxrquery = null;
-        String lxrqueryString = args.length > 0 ? args[0] : "LICENSE";
-
-        try {
-            String[] fields = {"content"};
-            lxrquery = new MultiFieldQueryParser(fields, lxranalyzer).parse(lxrqueryString);
-        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-            e.printStackTrace();
-        }
-        String path = "/opt/file_root/index_base/spdk_v17_10_1/";
-        Directory diskIndex = FSDirectory.open(Paths.get(path));
-
-
-
-
-        int hitsPerPage = 10;
-        //IndexReader reader = DirectoryReader.open(index);
-        IndexReader reader = DirectoryReader.open(diskIndex);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-        //searcher.search(query, collector);
-        /*
-        searcher.search(lxrquery, collector);
-        ScoreDoc[] hits = collector.topDocs().scoreDocs;
-
-        // 4. display results
-        System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". File " + d.get("filePath") + "\t" + d.get("fileName")  + "\t" + d.get("projectId")) ;
-        }
-
-        // reader can only be closed when there
-        // is no need to access the documents any more.
-        reader.close();
-        */
-        //String[] fields = {"content"};
-        //Query query = parser.parse("阿法狗");
-
-        String field="content";
-        //QueryParser parser = new QueryParser(Version.LATEST, field, lxranalyzer);
-        QueryParser parser = new QueryParser(field, lxranalyzer);
-        // 查询字符串
-        Query query=null;
-        try{
-            query = parser.parse("LICENSE");
+            if (commandLine.hasOption('q')) {
+                queryString = commandLine.getOptionValue('q');
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        if (query == null){
-            System.out.println("查询为空");
-            return ;
+    }
+
+    public static void main(String[] args) throws IOException {
+        handleOpthins(args);
+
+        hilightQuery();
+    }
+
+    private static void hilightShowDoc(Document doc, Analyzer analyzer, Highlighter highlighter, QueryScorer scorer)
+            throws IOException {
+        String contents = doc.get("content");
+
+        TokenStream tokenStream = TokenSources.getTokenStream(field, null, contents, analyzer, -1);
+        Fragmenter fragment = new SimpleSpanFragmenter(scorer);
+        highlighter.setTextFragmenter(fragment);
+        // 高亮news_title域
+        try {
+            String str = highlighter.getBestFragment(tokenStream, contents);// 获取高亮的片段，可以对其数量进行限制
+            if (str != null) {
+                // System.out.println((i + 1) + " . "+ doc.get("filePath") +"/"+
+                // doc.get("fileName")+ " 高亮 内容 ： "+str);
+                //System.out.println("# "+doc.get("filePath"));
+                System.out.println(str);
+                // tokenStream=TokenSources.getTokenStream( field, null,
+                // contents,lxranalyzer,-1);
+                // str=highlighter.getBestFragment(tokenStream,
+                // doc.get("content"));//获取高亮的片段，可以对其数量进行限制
+                // System.out.println("高亮 内容++："+str);
+            } else {
+                System.out.println("# 结果为空");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void hilightShowDocs(IndexSearcher searcher, Query query, Analyzer analyzer) throws IOException {
+        Query lxrquery = null;
+
+        try {
+            String[] fields = { "content" };
+            lxrquery = new MultiFieldQueryParser(fields, analyzer).parse(queryString);
+        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+            e.printStackTrace();
+        }
+        QueryScorer scorer = new QueryScorer(lxrquery, field);
+        SimpleHTMLFormatter fors = new SimpleHTMLFormatter("\033[36m", "\033[0m ");
+
+        Highlighter highlighter = new Highlighter(fors, scorer);
+
+        TopDocs topDocs = searcher.search(query, 10);
+
+        if (topDocs == null) {
+            return;
         }
 
-        QueryScorer scorer=new QueryScorer(lxrquery,field);
-        SimpleHTMLFormatter fors=new SimpleHTMLFormatter("<span style=\"color:red;\">", "</span>");
-        Highlighter highlighter=new Highlighter(fors, scorer);
+        System.out.println("= Hit Count：" + topDocs.totalHits);
 
-        System.out.println("query:" + query.toString());
-        // 返回前10条
-        TopDocs topDocs = searcher.search(query, 10);
-        if (topDocs != null) {
-            System.out.println("符合条件第文档总数：" + topDocs.totalHits);
+        for (int i = 0; i < topDocs.scoreDocs.length; i++) {
+            Document doc = searcher.doc(topDocs.scoreDocs[i].doc);
+            System.out.println((i + 1) + " . " + doc.get("filePath"));
+            //System.out.println((i + 1) + " . ");
+            // public static TokenStream getTokenStream(String field, Fields tvFields,
+            // String text, Analyzer analyzer,
+            // int maxStartOffset)
+            try {
+                hilightShowDoc(doc, analyzer, highlighter, scorer);
 
-            for (int i = 0; i < topDocs.scoreDocs.length; i++) {
-                Document doc = searcher.doc(topDocs.scoreDocs[i].doc);
-                System.out.println((i + 1) + " .  "+ doc.get("filePath") + " 当前文件名字" );
-                //public static TokenStream getTokenStream(String field, Fields tvFields, String text, Analyzer analyzer,
-                //int maxStartOffset)
-                try {
-                    String contents = doc.get("content");
-
-                    TokenStream tokenStream=TokenSources.getTokenStream( field, null, contents,lxranalyzer,-1);
-                    Fragmenter  fragment=new SimpleSpanFragmenter(scorer);
-                    highlighter.setTextFragmenter(fragment); 
-                    //高亮news_title域
-                    String str=highlighter.getBestFragment(tokenStream, contents);//获取高亮的片段，可以对其数量进行限制  
-                    if (str != null) {
-                        //System.out.println((i + 1) + " .  "+ doc.get("filePath") +"/"+ doc.get("fileName")+ " 高亮 内容 ： "+str);
-                        System.out.println((i + 1) + " .  "+ doc.get("filePath") + " 高亮 内容 ： "+str);
-                       // tokenStream=TokenSources.getTokenStream( field, null, contents,lxranalyzer,-1);
-                        //str=highlighter.getBestFragment(tokenStream, doc.get("content"));//获取高亮的片段，可以对其数量进行限制  
-                        //System.out.println("高亮 内容++："+str);
-                    }else{
-                        System.out.println((i + 1) + " . 结果为空");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    private static void hilightQuery() throws IOException {
+        Analyzer troobAnalyzer = new SourceFileAnalyzer();
+        Directory diskIndex = FSDirectory.open(Paths.get(file));
+        IndexReader reader = DirectoryReader.open(diskIndex);
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        String field = "content";
+        QueryParser parser = new QueryParser(field, troobAnalyzer);
+        // 查询字符串
+        Query query = null;
+        try {
+            query = parser.parse(queryString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (query == null) {
+            System.out.println("查询为空");
+            return;
+        }
+
+        System.out.println("= Query Field And Key Word[" + query.toString()+"]");
+        // 返回前10条
+        hilightShowDocs(searcher, query, troobAnalyzer);
+
         diskIndex.close();
         reader.close();
 
